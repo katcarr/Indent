7800818b8b0416e1ad314865951778b9172f2278
Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-03-27 15:37:45 -0700

    Add relationship of user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post<ActiveRecord::Base
   validates :content, :presence=> true
 
   has_many :comments
+  belongs_to :user
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,5 +5,6 @@ describe Post do
   it {should validate_presence_of :content }
 
   it {should have_many :comments }
+  it {should belong_to :user }
 
 end
